68b4e008697c8fe78ddf648f6f503c5dd85bae5f
Izzatbek <71765077+IzzatbekDadamirzayev@users.noreply.github.com>
2021-07-12 11:39:36 +0500

    First Upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const toggler_me = document.getElementsByName('toggler')
+const collapse = document.getElementById('collapse')
+
+
+// toggler_me.addEventListener('onclick', function () {
+//     collapse.classList.toggle('animation')
+// });
+
+function animationf(){
+    collapse.classList.toggle('animation')
+}
